ed0752c5d1e0886805ced2152de7ff865cc54162
dongchyeon <kimdonghyun90766@gmail.com>
2024-12-21 02:23:10 +0900

    Chore : MavenCentral Setting

--- app/src/main/java/com/dongchyeon/calendar/MainActivity.kt
+++ app/src/main/java/com/dongchyeon/calendar/MainActivity.kt
@@ -5,10 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.dongchyeon.calendar.ui.Calendar
 import com.dongchyeon.calendar.ui.theme.DongChyeonCalendarTheme
 import java.time.LocalDate
@@ -19,15 +26,34 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DongChyeonCalendarTheme {
-                var selectedDate by remember { mutableStateOf(LocalDate.now()) }
+                Scaffold(
+                    containerColor = Color.White
+                ) {
+                    Column(modifier = Modifier.padding(it)) {
+                        var selectedDate by remember { mutableStateOf(LocalDate.now()) }
 
-                Column {
-                    Calendar(
-                        selectedDate = selectedDate
-                    ) {
-                        selectedDate = it
+                        val events = listOf(
+                            CalendarEvent(
+                                date = LocalDate.now().plusDays(1),
+                                imgUrl = "https://picsum.photos/200/300",
+                                imgShape = CircleShape
+                            ),
+                            CalendarEvent(
+                                date = LocalDate.now().plusDays(2),
+                                imgUrl = "https://picsum.photos/200/300",
+                                imgShape = RoundedCornerShape(8.dp)
+                            ),
+                        )
+
+                        Calendar(
+                            selectedDate = selectedDate,
+                            events = events,
+                        ) {
+                            selectedDate = it
+                        }
                     }
                 }
+
             }
         }
     }
